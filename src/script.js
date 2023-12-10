888b13ed633af18c06acdcc2a117915426ca8cc5
AllDayDray <152281465+AllDayDray@users.noreply.github.com>
2023-12-10 17:33:26 +0000

    implemented a few scientific functions

--- src/script.js
+++ src/script.js
@@ -9,7 +9,15 @@ console.log('currentValue:', currentValue)
 const convertedValue = currentValue
 .replace("×", "*")
 .replace("÷", "/")
-.replace("%","*0.01");
+.replace("%","*0.01")
+.replace("sin", "Math.sin")
+.replace("cos", "Math.cos")
+.replace("ln", "Math.log")
+.replace("π", "Math.PI")
+.replace("log", "Math.log10")
+.replace("e", "Math.E")
+.replace("tan", "Math.tan")
+.replace("√", "Math.sqrt");
 console.log('currentValue:',convertedValue)
 const result = eval(convertedValue);
 currentValue = result.toString();
@@ -22,16 +30,26 @@ const button = buttons[i];
 button.addEventListener('click', function(){
 const value = button.innerText;
 
-if (value =="AC"){
-currentValue = "";
-display.value = currentValue;
-} else if(value== "=") {
-evaluateResult();
-} else {
-  currentValue += value;
-display.value = currentValue;
 
+try{
+
+    if (value =="AC"){
+        currentValue = "";
+        display.value = currentValue;
+        } else if(value== "=") {
+        evaluateResult();
+        } else {
+          currentValue += value;
+        display.value = currentValue; 
+        }
+
+
+} catch (error){
+    console.error(error);
+    currentValue= "ERROR";
+    display.value= currentValue
 }
+
   })
 }
 
